6cc27bb2431552fd2c83b31bb181e29daa229f47
Jamie Myland <Jamie.Myland@peterborough.ac.uk>
2019-11-21 10:50:43 +0000

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+
+const About = () => <div>About</div>;
+
+const Code = () => <div>Code</div>;
+
+const Contact = () => <div>Contact</div>;
+
+const Info = () => <div>info</div>;
+
+const MainMenu = () => {
+  return (
+    <div>
+      <Link to="/">
+        <button>home</button>
+      </Link>
+      <Link to="/about">
+        <button>About</button>
+      </Link>
+      <Link to="/code">
+        <button>code</button>
+      </Link>
+      <Link to="/contact">
+        <button>contact</button>
+      </Link>
+      <Link to="/info">
+        <button>info</button>
+      </Link>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <MainMenu />
+        </header>
+        <div style={{height:'500px'}}>
+          <Route exact path="/"><Home/></Route>
+          <Route exact path="/about"><About/></Route>
+          <Route exact path="/code"><Code/></Route>
+          <Route exact path="/contact"><Contact/></Route>
+          <Route exact path="/info"><Info/></Route>
+        </div>
+      </div>
+    </Router>
   );
 }
 
